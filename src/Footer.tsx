5e1ae5daf2e1b553b7f5d05d21290b6f1fa28ab3
Virgilio Maretto <info@posti.world>
2025-06-24 00:09:57 +0000

    ultime modifiche

---
+++ src/Footer.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <footer className="bg-[#e6f4f1] text-[#006747] p-4 mt-8">
+      <div className="container mx-auto text-center">
+        <p>
+          Powered by{" "}
+          <a
+            href="https://www.foodinnovationbroker.it/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-semibold text-[#006747] hover:underline"
+          >
+            Food Innovation Broker
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
