d9559339dd4f58e88beb8af0bd7c6fdd41a10393
vmaretto <info@posti.world>
2025-07-11 19:36:27 +0200

    Update questionnaire.ts

--- src/api/questionnaire.ts
+++ src/api/questionnaire.ts
@@ -1,7 +1,7 @@
 // src/api/questionnaire.ts
 
 // → punta direttamente al tuo Web App di Apps Script
-const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyNXywbOOuIeL1E8cnVfPQGoCDzeBE8_bx_y6RHwVcQaxe8z8i88VHgpjbCU281OPTV/exec";
+const WEBAPP_URL = "https://script.google.com/macros/s/AKfycbzbyZZUsQoUAhroIR2p-qVl0Gs7Ia5VEdC47wmpBYhOCq9vQXc6aeBw66eHhZ7JNSZT/exec";
 
 export const questionnaireAPI = {
   submit: async (data: Record<string, any>) => {
